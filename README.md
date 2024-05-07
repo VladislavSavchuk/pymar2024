4193e3f0c348935dcae40df66f2f10d26efacdd4
Владислав Савчук <uladzislau.sauchuk@redbarkgames.com>
2024-05-07 22:16:57 +0300

    update project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # pymar2024
+Python test automation course
+
+First name:
+Vladislav
+
+Last name:
+Savchuk
+
+Course description:
+Learning to automate testing in python language
---
+++ main.py
@@ -0,0 +1,12 @@
+"""Main module
+This module contains the 'main' function
+"""
+
+
+def main():
+    """This function prints the string 'Hello world!' to the console"""
+    print("Hello world!")
+
+
+if __name__ == "__main__":
+    main()
