c7c110d10e6aecbbba3d8a85957d57835e1a615e
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-05-11 12:03:09 +0300

    removed the unnecessary check in the function

--- homework5/name_correction.py
+++ homework5/name_correction.py
@@ -7,13 +7,8 @@ def name_correction():
     """This function corrects a string starting with an uppercase letter"""
     # создаем строку в разных регистрах
     capital = "pARiS"
-
-    # проводим проверку, что строка состоит только из букв
-    if capital.isalpha():
-        # исправляем строку при помощи метода title()
-        print(capital.title())
-    else:
-        print("В строке есть не только буквы")
+    # исправляем строку при помощи метода title()
+    print(capital.title())
 
 
 name_correction()
