64d5f46a7c6a23e47e9a94546e8f28d2128c3bcb
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-05-12 15:05:51 +0300

    modified the solution to the task in add_and_del_value_in_list.py

--- homework6/add_and_del_value_in_list.py
+++ homework6/add_and_del_value_in_list.py
@@ -1,16 +1,16 @@
 """Add and delete value in list.
-The program replaced and delete value in list
+The program add and delete value in list
 """
 
 
 def replace_and_del_value1():
-    """This function replaced and deleted value in list"""
+    """This function added and deleted value in list"""
     # Список значений
     simple_list = [1, 0.5, "Hi", -100, 5, 6, 0, 100, "Vlad", True]
     print(f"Старый список: {simple_list}")
 
-    # Заменяем элемент на 3 позиции в списке
-    simple_list[2] = 'Hello'
+    # Добавляем элемент на 3 позицию в списке
+    simple_list.insert(2, 999)
 
     # Удаляем элемент под индексом 6 в списке
     del simple_list[6]
@@ -24,15 +24,16 @@ replace_and_del_value1()
 
 
 def replace_and_del_value2():
-    """This function replaces and deletes value in list"""
+    """This function added and deleted value in list"""
     # Список значений
     simple_list = [1, 0.5, "Hi", -100, 5, 6, 0, 100, "Vlad", True]
     print(f"Старый список: {simple_list}")
 
-    # Заменяем "Hi" на "Hello" и удаляем 0
-    for index, value in enumerate(simple_list):
-        if value == "Hi":
-            simple_list[index] = 'Hello'
+    # Добавляем значение 999 в список и удаляем элемент под индексом 6
+    # переменная value заменена на знак подчеркивания "_"
+    for index, _ in enumerate(simple_list):
+        if index == 2:
+            simple_list.insert(2, 999)
         elif index == 6:
             del simple_list[index]
             break  # Прерываем цикл после удаления элемента
