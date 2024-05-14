84602547ea5bdb0316df2a5b4fe9db55f5ea2dfb
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-05-14 22:25:37 +0300

    fixed decisions tasks add_and_del_value_in_list.py and infinity_cycle.py

--- homework6/add_and_del_value_in_list.py
+++ homework6/add_and_del_value_in_list.py
@@ -23,23 +23,23 @@ def replace_and_del_value1():
 replace_and_del_value1()
 
 
-def replace_and_del_value2():
+def replace_and_del_value2(del_index):
     """This function added and deleted value in list"""
     # Список значений
     simple_list = [1, 0.5, "Hi", -100, 5, 6, 0, 100, "Vlad", True]
     print(f"Старый список: {simple_list}")
 
-    # Добавляем значение 999 в список и удаляем элемент под индексом 6
+    # Проходим по списку
     # переменная value заменена на знак подчеркивания "_"
     for index, _ in enumerate(simple_list):
         if index == 2:
+            # Добавляем значение 999 на 3 позицию в список
             simple_list.insert(2, 999)
-        elif index == 6:
-            del simple_list[index]
-            break  # Прерываем цикл после удаления элемента
+            # Удаляем элемент под индексом переданным в функцию (6)
+            del simple_list[del_index]
+            break   # Прерываем цикл после удаления элемента
 
     print(f"Новый список: {simple_list}")
-    print()
 
 
-replace_and_del_value2()
+replace_and_del_value2(6)
--- homework6/infinity_cycle.py
+++ homework6/infinity_cycle.py
@@ -5,11 +5,18 @@ This program identifies, leads the code to an infinite cycle.
 
 def is_infinity_cycle(a, b):
     """This function identifies an infinite cycle."""
-    while a != b:
+    while True:
         a += 1
         b -= 1
-    return a != b
+        if a == b:
+            # Возвращаем False, если цикл не бесконечен
+            return False
+        # Добавляем условие для проверки, что цикл стал бесконечным
+        if a > b:
+            break   # Если a стало больше b, прерываем цикл
+    # Возвращаем True, если цикл бесконечен
+    return True
 
 
-print(f"Этот цикл будет бесконечен. {is_infinity_cycle(2, 3)}")
-print(f"Этот цикл НЕ бесконечен. {is_infinity_cycle(2, 6)}")
+print("Для a = 2 и b = 6, результат:", is_infinity_cycle(2, 6))
+print("Для a = 2 и b = 3, результат:", is_infinity_cycle(2, 3))
