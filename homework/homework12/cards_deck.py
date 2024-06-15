8e298cd6c5c2686d04385c9c6ae248676ae91462
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-06-15 14:14:12 +0300

    add solutions tasks homework12

---
+++ homework/homework12/cards_deck.py
@@ -0,0 +1,64 @@
+"""Deck of cards
+This program contains a list of cards, knows how to shuffle them
+and allows the user to retrieve a card from the deck by its number.
+"""
+import random
+from itertools import product
+
+
+class Card:
+    """Create attributes of class"""
+    number_list = ([2, 3, 4, 5, 6, 7, 8, 9, 10] +
+                   ['jack', 'queen', 'king', 'ace'])
+    suit_list = ['hearts', 'diamonds', 'crosses', 'spades']
+
+    def __init__(self, number_list: list, mast_list: list):
+        """Initialize class object attributes,
+        the card has 2 attributes: value and suit"""
+        self.number_list = number_list
+        self.suit_list = mast_list
+
+    def __str__(self):
+        """Converts the output to a string"""
+        return f'{self.number_list} {self.suit_list}'
+
+
+class CardsDeck:
+    """This class contains three methods: create a deck of cards,
+    shuffle the cards, and return a card"""
+    def __init__(self):
+        """This method create cards with 2 attributes: number and suit
+        and packs it into a list"""
+        self.cards = []
+
+        for number, suit in product(Card.number_list, Card.suit_list):
+            self.cards.append(Card(number, suit))
+
+        self.cards.append('Joker Black')
+        self.cards.append('Joker Red')
+
+    def shuffle(self):
+        """This method shuffles the cards"""
+        random.shuffle(self.cards)
+
+    def get(self) -> str | None:
+        """This method checks range amount cards and return a card"""
+        if 0 <= card_number < len(self.cards):
+            return self.cards[card_number]
+        return None
+
+
+deck = CardsDeck()
+deck.shuffle()
+while True:
+    try:
+        card_number = int(input('Choose card from deck 54 cards: '))
+        card = deck.get()
+        if card:
+            print(f'Your card is: {card}')
+        else:
+            print('Invalid card number. '
+                  'Please choose a number card between 0 and 53')
+            card_number = int(input('Choose card from deck 54 cards again: '))
+    except ValueError:
+        print("Error! You need enter an integer")
---
+++ homework/homework12/exchange_currency.py
@@ -0,0 +1,91 @@
+"""Bank deposit.
+This program calculates the monthly capitalization of the user's deposit.
+Converts the original amount to the target currency.
+"""
+
+
+class Bank:
+    """Initialize bank deposit attributes"""
+    def __init__(self, deposit_amount: float, period: int, rate: float):
+        self.deposit_amount = deposit_amount  # starting deposit
+        self.period = period  # period of the deposit in months
+        self.rate = rate  # annual rate % divided by 100
+        self.currency = Currency
+        print('Deposit created')
+
+    def calculate_monthly_capitalization(self):
+        """This method calculates the monthly capitalization
+        of the customer's amount"""
+        result = round((self.deposit_amount * (1 + (self.rate / 100) / 12)
+                        ** self.period), 2)
+
+        return (f'Monthly capitalization of amount {self.deposit_amount}$ '
+                f'for {self.period} months equals to {result}$')
+
+    @staticmethod
+    def exchange_currency(from_currency, amount, to_currency='BYN'):
+        """This method return converted amount"""
+        return currency.convert(from_currency, amount, to_currency)
+
+
+class Customer:
+    """Initialize customer's"""
+    def __init__(self, name: str):
+        """This method outputs the name of the bank's customer"""
+        self.name = name
+
+
+class Person:
+    """This class contains method outputs names, amount
+    and currency person's"""
+    def __init__(self, name: str, curr: str, amount: int):
+        """TThis method stores currency and amount person's money"""
+        self.name = name
+        self.currency = curr
+        self.amount = amount
+
+
+class Currency:
+    """This class contains two methods: saving exchange rates,
+    currency conversion"""
+    def __init__(self):
+        """This method stores exchange rates in the rates dictionary"""
+        self.rates = {'USD': 3.19, 'EUR': 3.45, 'BYN': 1.0}
+
+    def convert(self, from_currency, amount, to_currency='BYN'):
+        """This method checks the current currency in list currency
+        and then converts it"""
+        if from_currency not in self.rates or to_currency not in self.rates:
+            raise ValueError("Invalid currency")
+
+        from_rate = self.rates[from_currency]
+        to_rate = self.rates[to_currency]
+
+        converted_amount = (amount / to_rate) * from_rate
+
+        return round(converted_amount, 2), to_currency
+
+
+person_1 = Customer('Lionel Messi')
+person_2 = Customer('LeBron James')
+print(person_1.name)
+deposit_person1 = Bank(1000000, 120, 10)
+print(deposit_person1.calculate_monthly_capitalization())
+print()
+print(person_2.name)
+deposit_person2 = Bank(999999.99, 60, 10)
+print(deposit_person2.calculate_monthly_capitalization())
+
+currency = Currency()
+
+person_1 = Person('Lionel', 'USD', 10)
+person_2 = Person('LeBron', 'EUR', 5)
+
+assert (Bank.exchange_currency(person_1.currency, person_1.amount) ==
+        (31.9, "BYN")), "Conversion error"
+assert (Bank.exchange_currency(person_2.currency, person_2.amount) ==
+        (17.25, "BYN")), "Conversion error"
+assert (Bank.exchange_currency(person_1.currency, person_1.amount, 'EUR') ==
+        (9.25, "EUR")), "Conversion error"
+assert (Bank.exchange_currency(person_2.currency, person_2.amount, 'USD') ==
+        (5.41, "USD")), "Conversion error"
