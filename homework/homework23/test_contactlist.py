0b06b827e4b26f5bf1eea038c1ade6140ed66631
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-07-24 23:48:07 +0300

    add contactlist

---
+++ homework/homework23/test_contactlist.py
@@ -0,0 +1,42 @@
+# https://thinking-tester-contact-list.herokuapp.com/
+
+# Login
+# 1. Enter login
+# email_input = "#email"
+# 2. Enter password
+# password_input = "#password"
+# 3. Click on "Submit" button
+# submit_button = "#submit"
+
+
+# Add contact
+# 1. Click on "Add a New Contact" button
+# name_input = "[id=add-contact]"
+# 2. Enter First name
+# first_name_input = "#firstName"
+# 3. Enter Last name
+# last_name_input = "#lastName"
+# 4. Click on "Submit" button
+# submit_button = "[id="submit"]"
+
+
+# Edit contact
+# 1. Click on Contact form
+# contact_form = "//*[@class="contactTable"]/tr[1]"
+# 2. Click on Edit Contact
+# edit_button = "[id="edit-contact"]"
+# 3. Enter new First name
+# first_name_input = "#firstName"
+# 4. Enter new Last name
+# last_name_input = "#lastName"
+# 5. Click on "Submit" button
+# submit_button = "[id="submit"]"
+
+
+# Remove contact
+# 1. Click on Contact form
+# contact_form = "//*[@class="contactTable"]/tr[1]"
+# 2. Click on Delete Contact button
+# delete_button = "[id="delete"]"
+# 3. Approve on Delete
+# press Enter on keyboard
