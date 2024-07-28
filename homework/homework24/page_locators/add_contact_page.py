8d1469dba108ae90f7ba6b192b4fef83405637fe
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-07-28 19:26:48 +0300

    add folder with locators

---
+++ homework/homework24/page_locators/add_contact_page.py
@@ -0,0 +1,21 @@
+"""Module for locator storage"""
+
+
+class AddContactLocators:
+    """The class contains locators to add a contact"""
+    # buttons
+    add_contact_btn = '[id="add-contact"]'
+    submit_btn = '[id="submit"]'
+    cancel_btn = '[id="cancel"]'
+    # forms
+    first_name_input = '[id="firstName"]'
+    last_name_input = '[id="lastName"]'
+    birthdate_input = '[id="birthdate"]'
+    email_input = '[id="email"]'
+    phone_input = '[id="phone"]'
+    street1_input = '[id="street1"]'
+    street2_input = '[id="street2"]'
+    city_input = '[id="city"]'
+    state_input = '[id="stateProvince"]'
+    postal_code_input = '[id="postalCode"]'
+    country_input = '[id="country"]'
---
+++ homework/homework24/page_locators/update_contact_details_page.py
@@ -0,0 +1,25 @@
+""" Module for locator storage """
+
+
+class EditContactLocators:
+    """ The class contains locators to edit a contact """
+    # contact row
+    contact_row = '//*[@class="contactTable"]/tr[1]'
+    # buttons
+    edit_contact_btn = '[id="edit-contact"]'
+    delete_contact_btn = '[id="delete"]'
+    return_to_contacts_btn = '[id="return"]'
+    submit_btn = '[id="submit"]'
+    cancel_btn = '[id="cancel"]'
+    # forms
+    first_name_form = '[id="firstName"]'
+    last_name_form = '[id="lastName"]'
+    birthdate_form = '[id="birthdate"]'
+    email_form = '[id="email"]'
+    phone_form = '[id="phone"]'
+    street1_form = '[id="street1"]'
+    street2_form = '[id="street2"]'
+    city_form = '[id="city"]'
+    state_form = '[id="stateProvince""]'
+    postal_code_form = '[id="postalCode"]'
+    country_form = '[id="country"]'
