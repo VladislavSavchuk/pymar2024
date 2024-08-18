8fc163b6750b7859baf5811560b41a17cda165d1
Vladislav Savchuk <vladislav.savvchuk@gmail.com>
2024-08-18 13:57:48 +0300

    update base methods with load token

--- homework/homework27/methods/api_base_methods.py
+++ homework/homework27/methods/api_base_methods.py
@@ -1,36 +1,48 @@
 """ This module contains base methods for the API """
 
 import requests
-from homework.homework27.conftest import HEADERS
+import json
+
+
+def load_token(config_file):
+    with open(config_file, 'r') as file:
+        config = json.load(file)
+    return config['API_TOKEN']
 
 
 class BaseAPI:
     """ Base API class """
-    def __init__(self, base_url):
+    def __init__(self, base_url, config_file='config.json'):
         """ Constructor """
         self.base_url = base_url
-        self.headers = HEADERS
+        self.token = load_token(config_file)
+        self.headers = {'Authorization': f'Bearer {self.token}',
+                        'Content-Type': 'application/json'}
+
+    def _get_headers(self):
+        """ Return headers for the request """
+        return self.headers
 
     def get(self, endpoint, params=None, timeout=10):
         """ Get method """
         url = f'{self.base_url}/{endpoint}'
-        return requests.get(url, headers=self.headers,
+        return requests.get(url, headers=self._get_headers(),
                             params=params, timeout=timeout)
 
     def post(self, endpoint, data, timeout=10):
         """ Post method """
         url = f'{self.base_url}/{endpoint}'
-        return requests.post(url, headers=self.headers,
+        return requests.post(url, headers=self._get_headers(),
                              json=data, timeout=timeout)
 
     def put(self, endpoint, data, timeout=10):
         """ Put method """
         url = f'{self.base_url}/{endpoint}'
-        return requests.put(url, headers=self.headers,
+        return requests.put(url, headers=self._get_headers(),
                             json=data, timeout=timeout)
 
     def delete(self, endpoint, timeout=10):
         """ Delete method """
         url = f'{self.base_url}/{endpoint}'
-        return requests.delete(url, headers=self.headers,
+        return requests.delete(url, headers=self._get_headers(),
                                timeout=timeout)
